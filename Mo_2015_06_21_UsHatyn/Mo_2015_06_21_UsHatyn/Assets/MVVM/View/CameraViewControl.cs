6e6fdce5bc6baabc9edf8a17347b666f36b9e8a4
RamazanovIgor <themirrortruth@gmail.com>
2015-05-26 01:22:54 +0900

    Интерфейс на камере
    Продолжение работы над интерфейсом на камере, добавил методы Set и Get
    для маркера. доступ к ним можно получить дописав using
    Mo_2015_06_21_UsHatyn; затем CameraViewControl.Instance.

--- Mo_2015_06_21_UsHatyn/Mo_2015_06_21_UsHatyn/Assets/MVVM/View/CameraViewControl.cs
+++ Mo_2015_06_21_UsHatyn/Mo_2015_06_21_UsHatyn/Assets/MVVM/View/CameraViewControl.cs
@@ -6,9 +6,29 @@ namespace Mo_2015_06_21_UsHatyn
 {
     public class CameraViewControl : UserControl
     {
+        public static CameraViewControl Instance;
+        public CameraViewControl()
+        {
+            Instance = this;
+        }
+
+        private Grid markerGrid;
+
         public void OnPostInit()
         {
-
+            markerGrid = FindName("MarkerGrid") as Grid;
         }
+        
+        public Vector2 GetMarkerPosition()
+        {
+            return new Vector2((markerGrid.Width - 100f) / 1280f * 100f, (800f - (markerGrid.Height - 100f)) / 800f * 100f);
+        }
+
+        public void SetMarkerPosition(float procentX, float procentY)
+        {
+            markerGrid.Width = 1280f * procentX / 100f + 100f;
+            markerGrid.Height = 100f + 800f - 800f * procentY / 100f;
+        }
+        
     }
 }
