3d85e76dbb4a91103e1eddd2e127d35a76632593
RamazanovIgor <themirrortruth@gmail.com>
2015-05-28 19:13:00 +0900

    Интерфейс на камере
    Сделал так, чтобы плашка была адаптивная и появлялся тот или иной тип
    плашки в зависимости от ее местоположения на экране. Теперь нет
    необходимости центрировать камеру при появлении миниплашки.

--- Mo_2015_06_21_UsHatyn/Mo_2015_06_21_UsHatyn/Assets/MVVM/View/CameraViewControl.cs
+++ Mo_2015_06_21_UsHatyn/Mo_2015_06_21_UsHatyn/Assets/MVVM/View/CameraViewControl.cs
@@ -20,31 +20,175 @@ namespace Mo_2015_06_21_UsHatyn
 
         public class MiniPanel
         {
-            public void ShowMiniPanel(string text)
+            public void ShowMiniPanel(float percentX, float percentY, string titleText)
             {
-                SetAboutPanelText(text);
-                CameraViewControl.Instance.aboutPanelShow.Begin();
+                HideMiniPanel();
+
+                if (percentX <= 50f && percentY <= 50f)
+                {
+                    leftBottomSetAboutPanelText(titleText);
+                    setLeftBottomPosition(percentX, percentY);
+                    leftBottomShowMiniPanel();
+                }
+                if (percentX > 50f && percentY <= 50f)
+                {
+                    rightBottomSetAboutPanelText(titleText);
+                    setRightBottomPosition(percentX, percentY);
+                    rightBottomShowMiniPanel();
+                }
+                if (percentX <= 50f && percentY > 50f)
+                {
+                    leftTopSetAboutPanelText(titleText);
+                    setLeftTopPosition(percentX, percentY);
+                    leftTopShowMiniPanel();
+                }
+                if (percentX > 50f && percentY > 50f)
+                {
+                    rightTopSetAboutPanelText(titleText);
+                    setRightTopPosition(percentX, percentY);
+                    rightTopShowMiniPanel();
+                }
             }
             public void HideMiniPanel()
             {
-                CameraViewControl.Instance.aboutPanelHide.Begin();
+                if (CameraViewControl.Instance.leftTopAboutPanelGrid.Visibility == Visibility.Visible)
+                {
+                    leftTopHideMiniPanel();
+                }
+                if (CameraViewControl.Instance.leftBottomAboutPanelGrid.Visibility == Visibility.Visible)
+                {
+                    leftBottomHideMiniPanel();
+                }
+                if (CameraViewControl.Instance.rightTopAboutPanelGrid.Visibility == Visibility.Visible)
+                {
+                    rightTopHideMiniPanel();
+                }
+                if (CameraViewControl.Instance.rightBottomAboutPanelGrid.Visibility == Visibility.Visible)
+                {
+                    rightBottomHideMiniPanel();
+                }
             }
-            private void SetAboutPanelText(string text)
+
+            private void leftTopShowMiniPanel()
+            {
+                CameraViewControl.Instance.leftTopAboutPanelShow.Begin();
+            }
+            private void leftTopHideMiniPanel()
+            {
+                CameraViewControl.Instance.leftTopAboutPanelHide.Begin();
+            }
+            private void leftTopSetAboutPanelText(string text)
             {
                 text = convertText(text);
-                CameraViewControl.Instance.aboutPanelTextBlock.Text = text;
+                CameraViewControl.Instance.leftTopAboutPanelTextBlock.Text = text;
                 int maxWord = lengthOfMaxWord(text);
                 int count = countOfWords(text);
                 if (maxWord > 8)
                 {
-                    CameraViewControl.Instance.aboutPanelGrid.Width = 136.667f + (maxWord - 8) * 10;
+                    CameraViewControl.Instance.leftTop.Width = 136.667f + (maxWord - 8) * 10;
                 }
                 else
                 {
-                    CameraViewControl.Instance.aboutPanelGrid.Width = 136.667f;
+                    CameraViewControl.Instance.leftTop.Width = 136.667f;
                 }
-                CameraViewControl.Instance.aboutPanelGrid.Height = 123 + 22 * (count - 1);
+                CameraViewControl.Instance.leftTop.Height = 123 + 22 * (count - 1);
             }
+            private void setLeftTopPosition(float percentX, float percentY)
+            {
+                CameraViewControl.Instance.leftTopAboutPanelGrid.Width = 1280f * percentX / 100f;
+                CameraViewControl.Instance.leftTopAboutPanelGrid.Height = 800f - 800f * percentY / 100f;
+            }
+            
+            private void leftBottomShowMiniPanel()
+            {
+                CameraViewControl.Instance.leftBottomAboutPanelShow.Begin();
+            }
+            private void leftBottomHideMiniPanel()
+            {
+                CameraViewControl.Instance.leftBottomAboutPanelHide.Begin();
+            }
+            private void leftBottomSetAboutPanelText(string text)
+            {
+                text = convertText(text);
+                CameraViewControl.Instance.leftBottomAboutPanelTextBlock.Text = text;
+                int maxWord = lengthOfMaxWord(text);
+                int count = countOfWords(text);
+                if (maxWord > 8)
+                {
+                    CameraViewControl.Instance.leftBottom.Width = 136.667f + (maxWord - 8) * 10;
+                }
+                else
+                {
+                    CameraViewControl.Instance.leftBottom.Width = 136.667f;
+                }
+                CameraViewControl.Instance.leftBottom.Height = 123 + 22 * (count - 1);
+            }
+            private void setLeftBottomPosition(float percentX, float percentY)
+            {
+                CameraViewControl.Instance.leftBottomAboutPanelGrid.Width = 1280f * percentX / 100f;
+                CameraViewControl.Instance.leftBottomAboutPanelGrid.Height = 800f - ( 800f * percentY / 100f + CameraViewControl.Instance.leftBottom.Height + 17);
+            }
+
+            private void rightTopShowMiniPanel()
+            {
+                CameraViewControl.Instance.rightTopAboutPanelShow.Begin();
+            }
+            private void rightTopHideMiniPanel()
+            {
+                CameraViewControl.Instance.rightTopAboutPanelHide.Begin();
+            }
+            private void rightTopSetAboutPanelText(string text)
+            {
+                text = convertText(text);
+                CameraViewControl.Instance.rightTopAboutPanelTextBlock.Text = text;
+                int maxWord = lengthOfMaxWord(text);
+                int count = countOfWords(text);
+                if (maxWord > 8)
+                {
+                    CameraViewControl.Instance.rightTop.Width = 136.667f + (maxWord - 8) * 10;
+                }
+                else
+                {
+                    CameraViewControl.Instance.rightTop.Width = 136.667f;
+                }
+                CameraViewControl.Instance.rightTop.Height = 123 + 22 * (count - 1);
+            }
+            private void setRightTopPosition(float percentX, float percentY)
+            {
+                CameraViewControl.Instance.rightTopAboutPanelGrid.Width = 1280f * percentX / 100f - CameraViewControl.Instance.rightTop.Width + 17;
+                CameraViewControl.Instance.rightTopAboutPanelGrid.Height = 800f - 800f * percentY / 100f;
+            }
+
+            private void rightBottomShowMiniPanel()
+            {
+                CameraViewControl.Instance.rightBottomAboutPanelShow.Begin();
+            }
+            private void rightBottomHideMiniPanel()
+            {
+                CameraViewControl.Instance.rightBottomAboutPanelHide.Begin();
+            }
+            private void rightBottomSetAboutPanelText(string text)
+            {
+                text = convertText(text);
+                CameraViewControl.Instance.rightBottomAboutPanelTextBlock.Text = text;
+                int maxWord = lengthOfMaxWord(text);
+                int count = countOfWords(text);
+                if (maxWord > 8)
+                {
+                    CameraViewControl.Instance.rightBottom.Width = 136.667f + (maxWord - 8) * 10;
+                }
+                else
+                {
+                    CameraViewControl.Instance.rightBottom.Width = 136.667f;
+                }
+                CameraViewControl.Instance.rightBottom.Height = 123 + 22 * (count - 1);
+            }
+            private void setRightBottomPosition(float percentX, float percentY)
+            {
+                CameraViewControl.Instance.rightBottomAboutPanelGrid.Width = 1280f * percentX / 100f - CameraViewControl.Instance.rightBottom.Width + 17;
+                CameraViewControl.Instance.rightBottomAboutPanelGrid.Height = 800f - (800f * percentY / 100f + CameraViewControl.Instance.rightBottom.Height + 17);
+            }
+
             private int lengthOfMaxWord(string text)
             {
                 int max = 0;
@@ -75,10 +219,10 @@ namespace Mo_2015_06_21_UsHatyn
             {
                 return new Vector2((CameraViewControl.Instance.markerGrid.Width - 100f) / 1280f * 100f, (800f - (CameraViewControl.Instance.markerGrid.Height - 100f)) / 800f * 100f);
             }
-            public void SetMarkerPosition(float procentX, float procentY)
+            public void SetMarkerPosition(float percentX, float percentY)
             {
-                CameraViewControl.Instance.markerGrid.Width = 1280f * procentX / 100f + 100f;
-                CameraViewControl.Instance.markerGrid.Height = 100f + 800f - 800f * procentY / 100f;
+                CameraViewControl.Instance.markerGrid.Width = 1280f * percentX / 100f + 100f;
+                CameraViewControl.Instance.markerGrid.Height = 100f + 800f - 800f * percentY / 100f;
             }
             public bool MarkerVisibility
             {
@@ -107,45 +251,86 @@ namespace Mo_2015_06_21_UsHatyn
             }
         }
 
+        /*-----LEFT TOP-------*/
+        private Grid leftTopAboutPanelGrid;
+        private Grid leftTop;
+        private TextBlock leftTopAboutPanelTextBlock;
+        private Button leftTopAboutPanelButton;
+        private Storyboard leftTopAboutPanelShow;
+        private Storyboard leftTopAboutPanelHide;
+        /*-----LEFT BOTTOM-------*/
+        private Grid leftBottomAboutPanelGrid;
+        private Grid leftBottom;
+        private TextBlock leftBottomAboutPanelTextBlock;
+        private Button leftBottomAboutPanelButton;
+        private Storyboard leftBottomAboutPanelShow;
+        private Storyboard leftBottomAboutPanelHide;
+        /*-----RIGHT TOP-------*/
+        private Grid rightTopAboutPanelGrid;
+        private Grid rightTop;
+        private TextBlock rightTopAboutPanelTextBlock;
+        private Button rightTopAboutPanelButton;
+        private Storyboard rightTopAboutPanelShow;
+        private Storyboard rightTopAboutPanelHide;
+        /*-----RIGHT BOTTOM-------*/
+        private Grid rightBottomAboutPanelGrid;
+        private Grid rightBottom;
+        private TextBlock rightBottomAboutPanelTextBlock;
+        private Button rightBottomAboutPanelButton;
+        private Storyboard rightBottomAboutPanelShow;
+        private Storyboard rightBottomAboutPanelHide;
+
         public MiniPanel MiniPanelObject;
         public Marker MarkerObject;
         private Grid markerGrid;
-        private Grid aboutPanelGrid;
-        private TextBlock aboutPanelTextBlock;
 
-        private TextBox testTextBox;
-        private Button testButton;
-        private Button bla;
-
-        private Storyboard aboutPanelShow;
-        private Storyboard aboutPanelHide;
+        private TextBox x;
+        private TextBox y;
+        private TextBox text;
 
         public void OnPostInit()
         {
+            leftTopAboutPanelGrid = FindName("LeftTopAboutPanelGrid") as Grid;
+            leftTop = FindName("LeftTop") as Grid;
+            leftTopAboutPanelTextBlock = FindName("LeftTopAboutPanelTextBlock") as TextBlock;
+            leftTopAboutPanelButton = FindName("LeftTopAboutPanelButton") as Button;
+            leftTopAboutPanelShow = FindResource("LeftTopAboutPanelShow") as Storyboard;
+            leftTopAboutPanelHide = FindResource("LeftTopAboutPanelHide") as Storyboard;
+
+            leftBottomAboutPanelGrid = FindName("LeftBottomAboutPanelGrid") as Grid;
+            leftBottom = FindName("LeftBottom") as Grid;
+            leftBottomAboutPanelTextBlock = FindName("LeftBottomAboutPanelTextBlock") as TextBlock;
+            leftBottomAboutPanelButton = FindName("LeftBottomAboutPanelButton") as Button;
+            leftBottomAboutPanelShow = FindResource("LeftBottomAboutPanelShow") as Storyboard;
+            leftBottomAboutPanelHide = FindResource("LeftBottomAboutPanelHide") as Storyboard;
+
+            rightTopAboutPanelGrid = FindName("RightTopAboutPanelGrid") as Grid;
+            rightTop = FindName("RightTop") as Grid;
+            rightTopAboutPanelTextBlock = FindName("RightTopAboutPanelTextBlock") as TextBlock;
+            rightTopAboutPanelButton = FindName("RightTopAboutPanelButton") as Button;
+            rightTopAboutPanelShow = FindResource("RightTopAboutPanelShow") as Storyboard;
+            rightTopAboutPanelHide = FindResource("RightTopAboutPanelHide") as Storyboard;
+
+            rightBottomAboutPanelGrid = FindName("RightBottomAboutPanelGrid") as Grid;
+            rightBottom = FindName("RightBottom") as Grid;
+            rightBottomAboutPanelTextBlock = FindName("RightBottomAboutPanelTextBlock") as TextBlock;
+            rightBottomAboutPanelButton = FindName("RightBottomAboutPanelButton") as Button;
+            rightBottomAboutPanelShow = FindResource("RightBottomAboutPanelShow") as Storyboard;
+            rightBottomAboutPanelHide = FindResource("RightBottomAboutPanelHide") as Storyboard;
+
             markerGrid = FindName("MarkerGrid") as Grid;
-            aboutPanelGrid = FindName("AboutPanelGrid") as Grid;
-            aboutPanelTextBlock = FindName("AboutPanelTextBlock") as TextBlock;
 
-            testTextBox = FindName("testTextBox") as TextBox;
-            testButton = FindName("testButton") as Button;
-            testButton.Click += testButton_Click;
-            bla = FindName("bla") as Button;
-            bla.Click += bla_Click;
+            x = FindName("x") as TextBox;
+            y = FindName("y") as TextBox;
+            text = FindName("text") as TextBox;
 
-            aboutPanelShow = FindResource("AboutPanelShow") as Storyboard;
-            aboutPanelHide = FindResource("AboutPanelHide") as Storyboard;
+            var button = FindName("button") as Button;
+            button.Click += button_Click;
         }
 
-        void bla_Click(object sender, RoutedEventArgs e)
+        void button_Click(object sender, RoutedEventArgs e)
         {
-            MiniPanelObject.HideMiniPanel();
+            MiniPanelObject.ShowMiniPanel(float.Parse(x.Text), float.Parse(y.Text), text.Text);
         }
-
-        void testButton_Click(object sender, RoutedEventArgs e)
-        {
-            MiniPanelObject.ShowMiniPanel(testTextBox.Text);
-        }
-
-        
     }
 }
